fdbb730f41ee64c0d8a50df67718a6a84f5108bb
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2025-04-14 00:09:12 +0700

    #8 add modal container

--- src/component/ModalContainer.jsx
+++ src/component/ModalContainer.jsx
@@ -1,7 +1,18 @@
+'use client';
+
+import { useRouter } from 'next/navigation'
+import Button from '@/component/Button';
+
 export default function ModalContainer({ children }) {
+    const router = useRouter();
     return (
-        <div className="modal-container">
-            {children}
+        <div className="w-full h-full flex justify-center items-center backdrop-blur-md bg-opacity-50 fixed top-0 left-0 z-50">
+            <div className="bg-white rounded-lg shadow-lg p-6 w-[70%] ">
+                {children}
+                <div className="mt-4 flex justify-end">
+                    <Button text="Close" onClick={() => router.back()} className="bg-blue-500 text-white px-4 py-2 rounded" />
+                </div>
+            </div>
         </div>
     );
 }
