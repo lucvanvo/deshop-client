8677271ddb2bebd9ab93639571f15240712729e2
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2025-04-14 11:09:19 +0700

    #48 show order details

--- src/app/order/[id]/page.js
+++ src/app/order/[id]/page.js
@@ -3,5 +3,7 @@ import OrderDetails from "@/component/OrderDetails";
 
 export default async function OrderPage({ params }) {
     const { id } = await params;
-    return <OrderDetails id={id} />;
+    return <div className="w-3/4 mx-auto mt-10">
+        <OrderDetails id={id} />
+    </div>;
 }
